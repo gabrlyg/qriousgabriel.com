85dc04a35c499d14e09c11f5e725009a5bf554ea
Gang Yang <qriousgabriel@gmail.com>
2021-11-16 23:28:16 +0000

    refactor: replaced css modules with global style components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
+import * as React from 'react'
 import type { AppProps } from 'next/app'
-import '@styles/normalize.css'
-import '@styles/style.css'
-import '@styles/prism-theme.css'
 import 'typeface-merriweather'
 import 'typeface-montserrat'
+import GlobalStyle from '@styles/GlobalStyles'
+import Normalize from '@styles/Normalize'
+import PrismTheme from '@styles/PrismTheme'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <Normalize />
+      <PrismTheme />
+      <Component {...pageProps} />
+    </React.Fragment>
+  )
 }
 
 export default App
---
+++ src/styles/index.ts
@@ -0,0 +1,3 @@
+export { default as GlobalStyle } from './GlobalStyles'
+export { default as Normalize } from './Normalize'
+export { default as PrismTheme } from './PrismTheme'
